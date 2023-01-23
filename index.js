dec0adcbdd4d0e1097b39581b5a24e0ae09f69ec
Jim Brennan <jim.j.brennan@gmail.com>
2023-01-23 18:38:08 -0500

    started messing around with the no joke button

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 
 
 //changes
+
+// adding event listener to the no joke button. it's not working of course. 
+// function addingClickEventToNoButton() {
+//     const noJokeButton = document.querySelector("no-joke-button");
+//     noJokeButton.addEventListener("click",
+//         (e) => {console.log("no-joke-button clicked")
+//         })
+// };
+// addingClickEventToNoButton();
