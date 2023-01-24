896ca68cbf5c1178b83c9e2624fd96b6e8835ceb
Jim Brennan <jim.j.brennan@gmail.com>
2023-01-24 14:09:36 -0500

    we're still figuring out git

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-
-//changes
-
 // adding event listener to the no joke button. it's not working of course. 
 // function addingClickEventToNoButton() {
 //     const noJokeButton = document.querySelector("no-joke-button");
@@ -28,8 +24,18 @@ noJokeButton.addEventListener("dblclick", (e) => {
 });
 
 
+noJokeButton.addEventListener("dblclick", (e) => {
+    setTimeout(function () { fartLong.play(); }, 1000);
+
+});
+
+
 //Awaiting sound effects for no-joke-button.
 
+// function randomJoke() {
+//     fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+//     .then(response => response.json())
+//     .then(randomJokeData => renderRandomJoke(randomJokeData))
 // function randomJoke() {
 //     fetch('https://official-joke-api.appspot.com/jokes/programming/random')
 //     .then(response => response.json())
@@ -38,3 +44,31 @@ noJokeButton.addEventListener("dblclick", (e) => {
 //     };
 // randomJoke();
 
+
+const topTenLocation = document.querySelector("#jokes-list > li");
+const topTenButton = document.querySelector("#new-joke-btn")
+
+topTenButton.addEventListener("click", () => {
+    console.log("jokes please")
+    
+    
+    fetch ('https://official-joke-api.appspot.com/jokes/programming/ten')
+    .then (r => r.json())
+    .then (data => makeTopTen (data))
+    
+   function makeTopTen(jokes) {
+       jokes.forEach((joke) => {
+           setup = joke.setup;
+           punchline = joke.punchline;
+           
+           combinedJoke = setup + " " + punchline;
+           
+           const li = document.createElement("li");
+           li.textContent = combinedJoke;
+           console.log(combinedJoke)
+           
+           topTenLocation.append(li);
+        })
+    }
+    
+})
