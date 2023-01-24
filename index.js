178b4c96fc30c0fc1bf967da941830ac63c4dd79
Jim Brennan <jim.j.brennan@gmail.com>
2023-01-24 09:24:45 -0500

    initial fetch request and no joke button are done

--- index.js
+++ index.js
@@ -10,3 +10,19 @@
 //         })
 // };
 // addingClickEventToNoButton();
+
+function randomJoke() {
+    fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+    .then(response => response.json())
+    .then(randomJokeData => renderRamens(randomJokeData))
+        
+    };
+randomJoke();
+
+const noJokeButton = document.querySelector("body > button.button.button2")
+noJokeButton.addEventListener("click", (e) => {
+console.log("Hello!!!")
+
+})
+
+//Awaiting sound effects for no-joke-button.
