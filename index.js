eee1a04112fd60e17588b03856ad0ef0792f2d04
Duane Grell <duanegrell@gmail.com>
2023-01-24 13:35:21 -0500

    added top ten with button

--- index.js
+++ index.js
@@ -28,3 +28,33 @@ function randomJoke() {
     };
 randomJoke();
 
+
+
+const topTenLocation = document.querySelector("#jokes-list > li");
+const topTenButton = document.querySelector("#new-joke-btn")
+
+topTenButton.addEventListener("click", () => {
+    console.log("jokes please")
+    
+    
+    fetch ('https://official-joke-api.appspot.com/jokes/programming/ten')
+    .then (r => r.json())
+    .then (data => makeTopTen (data))
+    
+   function makeTopTen(jokes) {
+       jokes.forEach((joke) => {
+           setup = joke.setup;
+           punchline = joke.punchline;
+           
+           combinedJoke = setup + " " + punchline;
+           
+           const li = document.createElement("li");
+           li.textContent = combinedJoke;
+           console.log(combinedJoke)
+           
+           topTenLocation.append(li);
+        })
+    }
+    
+})
+
